bbe290d387a4a6d1ffe2c24a24f9ac062a7ec781
LMFront-end <perfilgitlinamaria@gmail.com>
2021-10-02 19:16:32 -0500

    calculadora Js

---
+++ Js/app.js
@@ -0,0 +1,18 @@
+console.log('Hola');
+
+function sumar() {
+    const form = document.getElementById('form');
+
+    let operandoA = form['operandoA'];
+    let operandoB = form['operandoB'];
+
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+
+    if (isNaN(resultado))
+        resultado = `La operación no incluye números`;
+
+    document.getElementById('resultado').innerHTML = `Resultado = ${resultado}`
+
+    console.log(`El resultado es igual a:  ${resultado}`);
+
+}
